3023c7642c3d9a65c259edaf5f5c86e79649529a
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-11 22:25:39 +0800

    Program 3

---
+++ practice_python_program_batch_1/compute_sum_two_numbers.py
@@ -0,0 +1,9 @@
+# ask user to input two numbers
+input_number_one = int(input("Input First Number:"))
+input_number_two = int(input("Input Second Number:"))
+
+# calculate the sum of two inputted numbers
+sum = input_number_one + input_number_two
+
+# print sum
+print(sum)
