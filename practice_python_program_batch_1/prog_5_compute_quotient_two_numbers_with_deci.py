1d83e6c19a93f79e5710226745550c62930ccbf5
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-11 22:55:49 +0800

    Program 5

---
+++ practice_python_program_batch_1/prog_5_compute_quotient_two_numbers_with_deci.py
@@ -0,0 +1,9 @@
+# ask user to input two numbers
+input_number_one = int(input("Input First Number:"))
+input_number_two = int(input("Input Second Number:"))
+
+# calculate the quotient of two inputted numbers (with decimal)
+quotient = input_number_one / input_number_two
+
+# print quotient
+print(quotient)
