86c1cbbe84404e9bac4b8442042613ad86041853
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-11 23:38:02 +0800

    Program 10

--- practice_python_program_batch_1/prog_10_print_all_num_0_100_except_ending_0.py
+++ practice_python_program_batch_1/prog_10_print_all_num_0_100_except_ending_0.py
@@ -1,7 +1,7 @@
-# loop num from 0 100
+# loop num from 0 to 100
 for number in range(101):
 
-    # check if divisible ny 10
+    # check if divisible by 10
     if number % 10 != 0:
 
         #print number
