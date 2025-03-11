2d92004aeaebffd4252795c8f09834507931899f
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-11 23:11:57 +0800

    Program 7

---
+++ practice_python_program_batch_1/prog_7_compute_sum_of_10_num.py
@@ -0,0 +1,12 @@
+# initialize variable to store sum of all 10 numbers
+total = 0
+
+for i in range(10):
+    # ask user to input 10 numbers 
+    number = float(input(f"Please Enter Number {i + 1}:"))
+
+    # add the current number to the total sum
+    total += number
+    
+# print the total sum
+print(f"The sum of all 10 numbers are {total}:")
