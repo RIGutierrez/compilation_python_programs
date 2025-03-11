ffef40163f4af5bce9c52feb7aaf77f9514c75c9
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-11 23:04:22 +0800

    Program 6

---
+++ practice_python_program_batch_1/prog_6_first_num_raised_to_second_num.py
@@ -0,0 +1,9 @@
+# ask user to input two numbers
+input_number_one = int(input("Input First Number:"))
+input_number_two = int(input("Input Second Number:"))
+
+# raise the first number to the power of second number
+result = input_number_one ** input_number_two
+
+# print result
+print(result)
