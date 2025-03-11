8593ec419f166051b669fdcd44e4915e19736fe3
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-11 22:40:06 +0800

    Program 4

---
+++ practice_python_program_batch_1/prog_4_compute_product_two_numbers.py
@@ -0,0 +1,9 @@
+# ask user to input two numbers 
+input_number_one = int(input("Input First Number:"))
+input_number_two = int(input("Input Second Number:"))
+
+# calculate the product of two inputted numbers 
+product = input_number_one*input_number_two
+
+# print product
+print(product)
