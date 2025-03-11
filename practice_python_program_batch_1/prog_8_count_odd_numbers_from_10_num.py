37ac6f1e2ea7512543a566c5b3cbfaf9831c10fe
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-11 23:21:12 +0800

    Program 8

---
+++ practice_python_program_batch_1/prog_8_count_odd_numbers_from_10_num.py
@@ -0,0 +1,14 @@
+# initialize counter for odd num
+odd_count = 0
+
+for i in range(10):
+    # ask user to input a number
+    number = float(input(f"Please Enter Number {i + 1}:"))
+
+    # check if the number inputted is odd
+    if number % 2 != 0:
+        # increment count if odd
+        odd_count += 1
+
+# print the count of odd numbers        
+print(f"The number of odd numbers are {odd_count}")
