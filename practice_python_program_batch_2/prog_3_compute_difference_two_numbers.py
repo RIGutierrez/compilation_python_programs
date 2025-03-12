807f8d3ffb4033222fa4e7f70bc4f37a8312f401
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 15:57:06 +0800

    Program 3

--- practice_python_program_batch_2/prog_3_compute_difference_two_numbers.py
+++ practice_python_program_batch_2/prog_3_compute_difference_two_numbers.py
@@ -1,3 +1,9 @@
 # ask user to input two numbers
+input_number_one = int(input("Input First Number:"))
+input_number_two = int(input("Input Second Number:"))
+
 # calculate the difference of two numbers
+difference = input_number_one - input_number_two
+
 # print the difference
+print(difference)
