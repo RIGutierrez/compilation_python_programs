24edbe9122bf335e27a01121a32e05907c55791c
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 15:50:21 +0800

    Program 2

--- practice_python_program_batch_2/prog_2_check_number_if_not_equal.py
+++ practice_python_program_batch_2/prog_2_check_number_if_not_equal.py
@@ -1,4 +1,14 @@
 # ask user to input two numbers
+input_number_one = int(input("Input First Number:"))
+input_number_two = int(input("Input Second Number:"))
+
 # check if the inputted numbers are not the same
-# if not same, print "Not Equal"
-# if same, print "Equal"
+if input_number_one != input_number_two:
+    # if not same, print "Not Equal"
+    print("Not Equal")
+else:
+    # if same, print "Equal"
+    print("Equal")
+    
+
+
