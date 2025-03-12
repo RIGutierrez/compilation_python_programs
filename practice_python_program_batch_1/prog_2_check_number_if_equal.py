4256a8b0e434dd0f873712fc25fa296dd56df43f
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 19:26:44 +0800

    Program 2

--- practice_python_program_batch_1/prog_2_check_number_if_equal.py
+++ practice_python_program_batch_1/prog_2_check_number_if_equal.py
@@ -1,6 +1,6 @@
 # ask user to input
-input_number_one = int(input("Input First Number:"))
-input_number_two = int(input("Input Second Number:"))
+input_number_one = int(input("Input First Number: "))
+input_number_two = int(input("Input Second Number: "))
 
 # check if inputted two numbers are the same
 if input_number_one == input_number_two:
