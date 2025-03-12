09b4e4e733f0f7566395b447e56be0f9ac43fa34
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 16:44:03 +0800

    input working

--- practice_python_program_batch_2/prog_6_first_number_minus_all_remaining_number.py
+++ practice_python_program_batch_2/prog_6_first_number_minus_all_remaining_number.py
@@ -1,5 +1,12 @@
 # ask user to input for first number (starting value)
+inputs = int(input("Enter Number 1: "))
+
 # initialize result
+result = inputs
+
 # loop for asking input 
+for i in range(9):
+    numbers = int(input(f"Enter Number {i + 2}: "))
+    
 # subtract
 # print result
