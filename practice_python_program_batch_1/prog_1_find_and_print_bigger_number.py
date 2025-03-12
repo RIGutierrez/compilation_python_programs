80dd848101fc636e784b93ef6dc5266708142141
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 19:57:02 +0800

    Program 1 Final

--- practice_python_program_batch_1/prog_1_find_and_print_bigger_number.py
+++ practice_python_program_batch_1/prog_1_find_and_print_bigger_number.py
@@ -5,6 +5,6 @@ input_number_two = int(input("Input Second Number: "))
 # compare inputted two numbers
 if input_number_one > input_number_two:
     # print the bigger number
-    print("The larger number is: ", input_number_one)
+    print("The larger number is:", input_number_one)
 else:
     print("The larger number is:", input_number_two)  
