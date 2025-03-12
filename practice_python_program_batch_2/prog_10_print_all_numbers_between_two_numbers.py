e31b00bd50c23c183a1012180f6d984d9d5373ea
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 17:45:28 +0800

    swap values

--- practice_python_program_batch_2/prog_10_print_all_numbers_between_two_numbers.py
+++ practice_python_program_batch_2/prog_10_print_all_numbers_between_two_numbers.py
@@ -1,3 +1,11 @@
 # ask user to input two numbers
+input_number_one = int(input("Input First Number:"))
+input_number_two = int(input("Input Second Number:"))
+
 # check if input 1 is smaller compared to input 2
+if input_number_one > input_number_two:
+
+    # swap values to make input one smaller
+    input_number_one, input_number_two = input_number_two, input_number_one
+
 # loop to print numbers between two numbers
