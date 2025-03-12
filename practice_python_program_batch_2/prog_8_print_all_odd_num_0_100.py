79b8f40bc979b72643bc2925d3753bdef2509168
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 17:25:54 +0800

    Program 8

--- practice_python_program_batch_2/prog_8_print_all_odd_num_0_100.py
+++ practice_python_program_batch_2/prog_8_print_all_odd_num_0_100.py
@@ -1,8 +1,11 @@
-# use while function
-number = 1
+# use while funtion
+# initialize i = 1
+i = 1
 
-# initialize number = 1
-while number <= 100:
-    print(number)
 # while loop to print odd numbers until 100
-# increase by 2
+while i <= 100:
+    print(i)
+    # increase by 2
+    i += 2
+
+
