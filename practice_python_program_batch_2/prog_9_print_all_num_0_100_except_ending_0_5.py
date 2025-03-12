cf20e168ae31068fe81ead3148f5957ba524909b
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 17:37:09 +0800

    Program 9

--- practice_python_program_batch_2/prog_9_print_all_num_0_100_except_ending_0_5.py
+++ practice_python_program_batch_2/prog_9_print_all_num_0_100_except_ending_0_5.py
@@ -1,3 +1,10 @@
 # loop num from 0 to 100
-# check if ending with 0 and 5
-# print number
+for i in range(101):
+    # check if ending with 0 and 5
+    if i % 10 != 0 and i % 10 != 5:
+
+        # print number
+        print(i)
+
+
+ 
