f9be324f6ddc4e7525d92f2b70ee1207d4a9c7b8
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 15:38:57 +0800

    Program 1

--- practice_python_program_batch_2/prog_1_find_and_print_smaller_number.py
+++ practice_python_program_batch_2/prog_1_find_and_print_smaller_number.py
@@ -1,3 +1,10 @@
 # ask user to input two numbers
+input_number_one = int(input("Input First Number: "))
+input_number_two = int(input("Input Second Number: ")) 
+
 # compare inputted two numbers
-# print the smaller number
+if input_number_one < input_number_two:
+    # print the smaller number
+    print(f"The smaller number is: {input_number_one}")
+else:
+    print(f"The smaller number is: {input_number_two}")
