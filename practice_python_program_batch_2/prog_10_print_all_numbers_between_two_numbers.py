3e723f76c447373625b8a4f92a6339ff275d0fb9
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 17:48:26 +0800

    loop working

--- practice_python_program_batch_2/prog_10_print_all_numbers_between_two_numbers.py
+++ practice_python_program_batch_2/prog_10_print_all_numbers_between_two_numbers.py
@@ -9,3 +9,5 @@ if input_number_one > input_number_two:
     input_number_one, input_number_two = input_number_two, input_number_one
 
 # loop to print numbers between two numbers
+for number in range(input_number_one + 1, input_number_two):
+    print(number)
