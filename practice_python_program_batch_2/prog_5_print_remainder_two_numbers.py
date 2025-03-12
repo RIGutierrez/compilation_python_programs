cb176cea793d0133a25f82d33a283dd1ee6b74c2
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 16:27:29 +0800

    Program 5

--- practice_python_program_batch_2/prog_5_print_remainder_two_numbers.py
+++ practice_python_program_batch_2/prog_5_print_remainder_two_numbers.py
@@ -1,4 +1,18 @@
 # ask user to input two numbers
+input_number_one = int(input("Input First Number:"))
+input_number_two = int(input("Input Second Number:"))
+
 # check if inputted second number is not 0
+if input_number_two == 0:
+    print("Dividing a number by 0 is not allowed.")
+
 # if not 0, proceed
-# print remainder
+else:
+    quotient_remainder = input_number_one % input_number_two
+
+    #print remainder
+    print(quotient_remainder)
+
+    
+
+
