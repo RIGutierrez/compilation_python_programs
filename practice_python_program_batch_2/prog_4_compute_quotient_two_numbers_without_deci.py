1f98895556fb57ae8c7a4ec67abbebc4307cdef4
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 16:05:18 +0800

    Program 4

--- practice_python_program_batch_2/prog_4_compute_quotient_two_numbers_without_deci.py
+++ practice_python_program_batch_2/prog_4_compute_quotient_two_numbers_without_deci.py
@@ -1,3 +1,9 @@
 # ask user to input two numbers
+input_number_one = int(input("Input First Number:"))
+input_number_two = int(input("Input Second Number:"))
+
 # calculate the quotient of inputted two numbers (w/o decimal)
+quotient_without_decimal = input_number_one // input_number_two
+
 # print quotient without decimal
+print(quotient_without_decimal)
