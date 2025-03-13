9486fa3b9072a1206aa36e93ba3b593a43eb8ef6
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-13 21:40:28 +0800

    Store value and break invalid

--- practice_python_program_batch_3/prog_4_display_lowest_number.py
+++ practice_python_program_batch_3/prog_4_display_lowest_number.py
@@ -1,5 +1,17 @@
 # list to store value
-# ask user to input numbers
-# store number to numbers
+numbers = []
+
+while True:
+    try:
+
+        # ask user to input numbers
+        number = int(input("Please input a number: "))
+
+        # store number to numbers
+        numbers.append(number)
+    except ValueError:
+        break
+
+
 # break if invalid
 # check lowest number
