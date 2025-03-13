fd743da453da6c2a6351bf5f6f666d32b2fb73f2
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 00:23:04 +0800

    Program 10

--- practice_python_program_batch_2/prog_10_print_all_numbers_between_two_numbers.py
+++ practice_python_program_batch_2/prog_10_print_all_numbers_between_two_numbers.py
@@ -1,6 +1,6 @@
 # ask user to input two numbers
-input_number_one = int(input("Input First Number:"))
-input_number_two = int(input("Input Second Number:"))
+input_number_one = int(input("Input First Number: "))
+input_number_two = int(input("Input Second Number: "))
 
 # check if input 1 is smaller compared to input 2
 if input_number_one > input_number_two:
@@ -8,8 +8,11 @@ if input_number_one > input_number_two:
     # swap values to make input one smaller
     input_number_one, input_number_two = input_number_two, input_number_one
 
-# loop to print numbers between two numbers
-for number in range(input_number_one + 1, input_number_two):
-    # print number
-    print(number)
-
+# check if both numbers are the same
+if input_number_one == input_number_two:
+    print("No numbers between, both inputs are the same.")
+else:
+    # loop to print numbers between two numbers
+    for number in range(input_number_one + 1, input_number_two):
+        # print number
+        print(number)
