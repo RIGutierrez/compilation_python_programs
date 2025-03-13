a558884f0a12e7c07e5ba69e867dc042654d4c6a
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-13 17:42:22 +0800

    Program 3

--- practice_python_program_batch_3/prog_2_print_num_if_duplicate_print_first_entry.py
+++ practice_python_program_batch_3/prog_2_print_num_if_duplicate_print_first_entry.py
@@ -1,9 +1,18 @@
 # list to store value
+numbers = []
 
 # ask user to input numbers using loop
 for i in range(10):
     number = int(input(f"Enter number {i + 1}: "))
-    
+    numbers.append(number)
+
 # new list for inputs unique numbers
+unique_numbers = []
+
 # remove duplicates and print only the first entry
-# print numbers
+for num in numbers:
+    if num not in unique_numbers:
+        unique_numbers.append(num)
+
+        # print numbers
+        print(num)
