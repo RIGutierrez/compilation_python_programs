e7b219b9435a25354b36713c0baac6f73a9ff6ca
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 00:36:04 +0800

    Program 6

--- practice_python_program_batch_1/prog_6_first_num_raised_to_second_num.py
+++ practice_python_program_batch_1/prog_6_first_num_raised_to_second_num.py
@@ -1,9 +1,13 @@
 # ask user to input two numbers
-input_number_one = int(input("Input First Number:"))
-input_number_two = int(input("Input Second Number:"))
+input_number_one = int(input("Input First Number: "))
+input_number_two = int(input("Input Second Number: "))
 
-# raise the first number to the power of second number
-result = input_number_one ** input_number_two
+# check if input is zero and exponent is negative
+if input_number_one == 0 and input_number_two < 0:
+    print("0 cannot be raised to a negative power.")
+else:
+    # raise the first number to the power of second number
+    result = input_number_one ** input_number_two
 
-# print result
-print(result)
+    # print result
+    print(result)
