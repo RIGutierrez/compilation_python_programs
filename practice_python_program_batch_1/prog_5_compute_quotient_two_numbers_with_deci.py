97c9e4a9c5387147275b99ee0e15cac9acdaba74
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 00:32:14 +0800

    Program 5

--- practice_python_program_batch_1/prog_5_compute_quotient_two_numbers_with_deci.py
+++ practice_python_program_batch_1/prog_5_compute_quotient_two_numbers_with_deci.py
@@ -1,9 +1,14 @@
 # ask user to input two numbers
-input_number_one = int(input("Input First Number:"))
-input_number_two = int(input("Input Second Number:"))
+input_number_one = int(input("Input First Number: "))
+input_number_two = int(input("Input Second Number: "))
 
-# calculate the quotient of two inputted numbers (with decimal)
-quotient = input_number_one / input_number_two
+# check if second number is 0
+if input_number_two == 0:
+    # 0 is not allowed
+    print("Dividing a number by 0 is not allowed.")
+else:
+    # calculate the quotient of two inputted numbers (with decimal)
+    quotient = input_number_one / input_number_two
 
 # print quotient
 print(quotient)
