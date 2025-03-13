68d3e873cc9570c8397c3d8b1ac27739b087aead
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-13 21:46:48 +0800

    lowest number input print

--- practice_python_program_batch_3/prog_4_display_lowest_number.py
+++ practice_python_program_batch_3/prog_4_display_lowest_number.py
@@ -9,9 +9,12 @@ while True:
 
         # store number to numbers
         numbers.append(number)
+
+    # break if invalid
     except ValueError:
         break
 
+# check lowest number   
+if numbers: 
+    print(min(numbers))
 
-# break if invalid
-# check lowest number
