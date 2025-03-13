6d5f9f75cb2680d72ed3ed1c50573d32c63c7e92
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-13 17:04:05 +0800

    listing

--- practice_python_program_batch_3/prog_1_print_num_if_no_duplicate.py
+++ practice_python_program_batch_3/prog_1_print_num_if_no_duplicate.py
@@ -1,5 +1,17 @@
 # ask user to input 10 numbers using loop
 for number in range(10):
     number = int(input(f"Enter number {number + 1}: "))
+
 # new list for input appear only once
-# print no duplicate numbers
+unique_number = []
+duplicate_number = []
+
+for element in number:
+    if element not in unique_number:
+        unique_number.append(element)
+    elif element not in duplicate_number:
+        duplicate_number.append(element)    
+# print no duplicate 
+
+print(unique_number)
+print(duplicate_number)
