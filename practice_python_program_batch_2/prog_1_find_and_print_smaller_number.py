66ae6cd9f4bb3040afb08549732d4e523b8e13ab
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 00:09:21 +0800

    Program 1

--- practice_python_program_batch_2/prog_1_find_and_print_smaller_number.py
+++ practice_python_program_batch_2/prog_1_find_and_print_smaller_number.py
@@ -6,5 +6,7 @@ input_number_two = int(input("Input Second Number: "))
 if input_number_one < input_number_two:
     # print the smaller number
     print(f"The smaller number is: {input_number_one}")
-else:
+elif input_number_one > input_number_two:
     print(f"The smaller number is: {input_number_two}")
+else:
+    print("Both numbers are equal.")
