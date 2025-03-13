c6944b5fef2079fc8dad041b2f90d5f0741a940c
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-13 23:44:46 +0800

    Program 5

--- practice_python_program_batch_4/prog_5_display_average_if_invalid.py
+++ practice_python_program_batch_4/prog_5_display_average_if_invalid.py
@@ -1,5 +1,24 @@
 # list to store values
-# ask user to input numbers
-# increment count for sum
-# break if invalid
+numbers = []
+count = 0
+total_sum = 0
+
+while True:
+    try:
+        # ask user to input numbers
+        number = int(input("Please input a number: "))
+        
+        # increment count for total sum
+        total_sum += number
+        count += 1
+
+    # break if invalid
+    except ValueError:
+        break
+
 # display average
+if count > 0:
+    average = total_sum / count 
+    print(f"The average of the inputted numbers is: {average:.2f}")
+else:
+    print("No value inputted")
