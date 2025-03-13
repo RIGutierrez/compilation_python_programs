fb4915f7600d194c589b5bb36dd5f82b0fa4df21
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 00:17:36 +0800

    Program 7

--- practice_python_program_batch_2/prog_7_count_even_numbers_from_10_num.py
+++ practice_python_program_batch_2/prog_7_count_even_numbers_from_10_num.py
@@ -4,7 +4,8 @@ even_count = 0
 for i in range(10):
     # ask user to input a number
     number = float(input(f"Please Enter Number {i + 1}: "))
-    #check if the number inputtes is even
+
+    # check if the number inputtes is even
     if number % 2 == 0:
         # increment count if even
         even_count += 1
